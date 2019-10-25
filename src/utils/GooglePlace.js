397a1c3ca47898c7d4dc3f96b87d452778e5a3e4
Dorian Guilmain <guilmain.dorian@gmail.com>
2019-10-25 09:27:10 +0200

    :construction: Get details from GooglePlace

--- src/utils/GooglePlace.js
+++ src/utils/GooglePlace.js
@@ -1,18 +1,30 @@
-/* address_component, adr_address, formatted_address, geometry, icon, name, permanently_closed, photo, type, url, utc_offset, vicinity */
-
-const enabled = false;
+const enabled = true;
 
 const GooglePlace = request => {
+  const places = [];
   if (enabled) {
     const service = new window.google.maps.places.PlacesService(request.map);
     service.nearbySearch(request, (results, status) => {
       if (status === window.google.maps.places.PlacesServiceStatus.OK) {
-        for (let i = 0; i < results.length; i += 1) {
-          console.log(results[i]);
+        // TODO: Delay for loop
+        for (let i = 0; i < 3; i += 1) {
+          // Get data for Basic Plan only
+          // See https://developers.google.com/maps/billing/gmp-billing#basic-data
+          const detailsRequest = {
+            placeId: results[i].place_id,
+            fields: ['address_component', 'adr_address', 'formatted_address', 'geometry', 'icon', 'name', 'permanently_closed', 'photo', 'type', 'url', 'utc_offset', 'vicinity']
+          };
+          service.getDetails(detailsRequest, (place, statusDetails) => {
+            const result = results[i];
+            if (statusDetails === window.google.maps.places.PlacesServiceStatus.OK) {
+              places.push({...place, place_id: result.place_id, rating: result.rating, price_level: result.price_level, user_ratings_total: result.user_ratings_total });
+            }
+          });
         }
       }
     });
   }
+  return places;
 };
 
 export default GooglePlace;
