c3f871a9bd1599ae303a7f686eec12209f2a3c83
Dorian Guilmain <guilmain.dorian@gmail.com>
2019-10-25 09:27:42 +0200

    :sparkles: Done adapters

---
+++ src/model/YelpAdapter.js
@@ -0,0 +1,31 @@
+const YelpAdapter = data => {
+  console.log(`YelpAdapter: ${data.length}`);
+  console.log(data);
+  return data.map(item => {
+    return {
+      id: item.id,
+      alias: item.alias,
+      name: item.name,
+      imageUrl: item.image_url,
+      reviewCount: item.review_count,
+      rating: item.rating,
+      categories: item.categories.map(category => category.title),
+      latitude: item.coordinates.latitude,
+      longitude: item.coordinates.longitude,
+      priceLevel: item.price,
+      displayAddress: item.location.display_address.join(" "),
+      reviews: item.reviews.map(review => {
+        return {
+          id: review.id,
+          name: review.user.name,
+          imageUrl: review.user.image_url,
+          rating: review.rating,
+          text: review.text,
+          timeCreated: review.time_created
+        };
+      })
+    };
+  })
+};
+
+export default YelpAdapter;
